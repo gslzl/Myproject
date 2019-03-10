278d02b172707378ebd987b8028a7cec4fcd9f08
Jason <1594485527@qq.com>
2019-03-10 10:33:48 +0800

    完成头像上传以及界面中同步更新

--- src/main/java/com/example/myproject/HomepageActivity.java
+++ src/main/java/com/example/myproject/HomepageActivity.java
@@ -2,40 +2,47 @@ package com.example.myproject;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.FileProvider;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.blankj.utilcode.util.ConvertUtils;
+import com.blankj.utilcode.util.SPUtils;
 import com.example.myproject.fragments.AddFragment;
 import com.example.myproject.fragments.HomeFragment;
 import com.example.myproject.fragments.NewsFragment;
+import com.example.myproject.utils.MyUtils;
 import com.facebook.rebound.SpringConfig;
 import com.jpeng.jpspringmenu.MenuListener;
 import com.jpeng.jpspringmenu.SpringMenu;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.net.URI;
+import java.io.FileOutputStream;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static com.example.myproject.MyApp.AVATAR_FILE_NAME;
 
-public class HomepageActivity extends AppCompatActivity  {
+
+public class HomepageActivity extends AppCompatActivity implements View.OnClickListener, MenuListener {
 
 
     @InjectView(R.id.home)
@@ -46,6 +53,20 @@ public class HomepageActivity extends AppCompatActivity  {
     ImageView news;
 
 
+    private static final int REQUEST_PERMISSION = 0;
+    private static final int FINISH_CROP = 1;
+    private static final int CROP_PHOTO = 2;
+
+
+    HomeFragment homeFragment;
+    NewsFragment newsFragment;
+    SpringMenu menu;
+
+    LinearLayout layoutOrderManage;
+    LinearLayout layoutCollection;
+    LinearLayout layoutCustomerService;
+    LinearLayout layoutSetting;
+    CircleImageView userAvatar;
 
 
     @Override
@@ -53,28 +74,60 @@ public class HomepageActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         ButterKnife.inject(this);
-        HomeReplace(new HomeFragment());
+        homeFragment = new HomeFragment();
+        newsFragment = new NewsFragment();
+        initMenu();
+        HomeReplace(homeFragment);
     }
 
+    public void openMenu() {
+        menu.openMenu();
+    }
+
+    void initMenu() {
+
+        menu = new SpringMenu(this, R.layout.menu_mine);
+        menu.setMenuListener(this);
+        menu.setChildSpringConfig(SpringConfig.fromOrigamiTensionAndFriction(5, 5));
+        menu.setFadeEnable(true);
+        menu.setDirection(SpringMenu.DIRECTION_LEFT);
+        menu.setDragOffset(0.4f);
 
 
+        userAvatar = menu.getMenuView().findViewById(R.id.civ_avatar);
+        layoutOrderManage = menu.getMenuView().findViewById(R.id.layout_order_manage);
+        layoutCollection = menu.getMenuView().findViewById(R.id.layout_collection);
+        layoutCustomerService = menu.getMenuView().findViewById(R.id.layout_customer_service);
+        layoutSetting = menu.getMenuView().findViewById(R.id.layout_setting);
+
+        layoutSetting.setOnClickListener(this);
+        layoutCollection.setOnClickListener(this);
+        layoutCustomerService.setOnClickListener(this);
+        layoutOrderManage.setOnClickListener(this);
+        userAvatar.setOnClickListener(this);
+        File file = new File(Environment.getExternalStorageDirectory(), AVATAR_FILE_NAME);
+        if (file.exists())
+            userAvatar.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/" + AVATAR_FILE_NAME));
+
+    }
 
 
     @OnClick({R.id.home, R.id.add, R.id.news})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.home:
-                HomeReplace(new HomeFragment());
+                HomeReplace(homeFragment);
                 break;
             case R.id.add:
                 HomeReplace(new AddFragment());
                 break;
             case R.id.news:
-                HomeReplace(new NewsFragment());
+                HomeReplace(newsFragment);
                 break;
         }
     }
-    public void HomeReplace(Fragment fragment){
+
+    public void HomeReplace(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.home_fragment, fragment);
@@ -84,8 +137,132 @@ public class HomepageActivity extends AppCompatActivity  {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-//        return menu.dispatchTouchEvent(ev);
         return super.dispatchTouchEvent(ev);
     }
+
+
+    @Override
+    public void onMenuOpen() {
+
+    }
+
+    @Override
+    public void onMenuClose() {
+
+    }
+
+    @Override
+    public void onProgressUpdate(float value, boolean bouncing) {
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_order_manage:
+                break;
+            case R.id.layout_collection:
+                break;
+            case R.id.layout_customer_service:
+                break;
+            case R.id.layout_setting:
+                break;
+            case R.id.civ_avatar:
+                openAlbum();
+
+                break;
+        }
+    }
+
+
+    public void openAlbum() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, CROP_PHOTO);
+            } else {
+                requestPermissions(perms, REQUEST_PERMISSION);
+
+            }
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, CROP_PHOTO);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CROP_PHOTO:
+                startActivityForResult(cropPhoto(data.getData()), FINISH_CROP);
+                break;
+            case FINISH_CROP:
+                try {
+                    Bitmap bitmap = data.getParcelableExtra("data");
+                    homeFragment.setEntranceAvatar(bitmap);
+                    newsFragment.setEntranceAvatar(bitmap);
+                    userAvatar.setImageBitmap(bitmap);
+
+                    File file = new File(Environment.getExternalStorageDirectory(), AVATAR_FILE_NAME);
+                    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file));
+                    //压缩
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                    outputStream.flush();
+                    outputStream.close();
+
+                    MyUtils.uploadAvatar(file, SPUtils.getInstance().getString("str_login_number"));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+
+    private Intent cropPhoto(Uri oriImageUri) {
+
+        try {
+
+            Intent intent = new Intent("com.android.camera.action.CROP");
+
+            intent.putExtra("crop", true);
+            intent.putExtra("aspectX", 1);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("outputX", ConvertUtils.dp2px(90));
+            intent.putExtra("outputY", ConvertUtils.dp2px(90));
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+            intent.setDataAndType(oriImageUri, "image/*");
+            intent.putExtra("noFaceDetection", true);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+            return intent;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
 }
--- src/main/java/com/example/myproject/fragments/HomeFragment.java
+++ src/main/java/com/example/myproject/fragments/HomeFragment.java
@@ -1,36 +1,27 @@
 package com.example.myproject.fragments;
 
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
+
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
-import com.blankj.utilcode.util.ConvertUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.example.myproject.HomepageActivity;
+import com.example.myproject.MyApp;
 import com.example.myproject.R;
 import com.example.myproject.bean.BannerBean;
-import com.facebook.rebound.SpringConfig;
 import com.google.gson.Gson;
-import com.jpeng.jpspringmenu.MenuListener;
-import com.jpeng.jpspringmenu.SpringMenu;
+
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 import com.lzy.okgo.model.Response;
@@ -38,7 +29,6 @@ import com.example.myproject.adapter.ProductRecycviewAdapter;
 import com.recker.flybanner.FlyBanner;
 
 import java.io.File;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,22 +39,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * A simple {@link Fragment} subclass.
  */
 
-public class HomeFragment extends Fragment implements MenuListener, View.OnClickListener {
+public class HomeFragment extends Fragment {
     View view;
     String getUrl;
     String inforUrl;
 
-    private static final int REQUEST_PERMISSION = 0;
-    private static final int FINISH_CROP = 1;
-    private static final int CROP_PHOTO = 2;
 
-    SpringMenu menu;
-
-    LinearLayout layoutOrderManage;
-    LinearLayout layoutCollection;
-    LinearLayout layoutCustomerService;
-    LinearLayout layoutSetting;
-    CircleImageView userAvatar;
     CircleImageView entranceAvatar;
 
     List<String> imgesUrl = new ArrayList<>();
@@ -73,47 +53,29 @@ public class HomeFragment extends Fragment implements MenuListener, View.OnClick
 
     RecyclerView product;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    void initMenu() {
-
-        menu = new SpringMenu(getActivity(), R.layout.menu_mine);
-        menu.setMenuListener(this);
-        menu.setChildSpringConfig(SpringConfig.fromOrigamiTensionAndFriction(5, 5));
-        menu.setFadeEnable(true);
-        menu.setDirection(SpringMenu.DIRECTION_LEFT);
-        menu.setDragOffset(0.4f);
-
-
-        userAvatar = menu.getMenuView().findViewById(R.id.civ_avatar);
-        layoutOrderManage = menu.getMenuView().findViewById(R.id.layout_order_manage);
-        layoutCollection = menu.getMenuView().findViewById(R.id.layout_collection);
-        layoutCustomerService = menu.getMenuView().findViewById(R.id.layout_customer_service);
-        layoutSetting = menu.getMenuView().findViewById(R.id.layout_setting);
-
-        layoutSetting.setOnClickListener(this);
-        layoutCollection.setOnClickListener(this);
-        layoutCustomerService.setOnClickListener(this);
-        layoutOrderManage.setOnClickListener(this);
-        userAvatar.setOnClickListener(this);
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        LogUtils.i("初始化oncreate");
+        super.onCreate(savedInstanceState);
 
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
         banner = view.findViewById(R.id.banner);
-        initMenu();
         entranceAvatar = view.findViewById(R.id.head_image);
+        File file = new File(Environment.getExternalStorageDirectory(), MyApp.AVATAR_FILE_NAME);
+        if (file.exists())
+            entranceAvatar.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/" + MyApp.AVATAR_FILE_NAME));
+
         entranceAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                menu.openMenu();
+                HomepageActivity homepageActivity = (HomepageActivity) getActivity();
+                homepageActivity.openMenu();
             }
         });
 //        initBanner();
@@ -121,15 +83,14 @@ public class HomeFragment extends Fragment implements MenuListener, View.OnClick
 
         ArrayList list = new ArrayList();
         for (int i = 0; i < 10; i++) {
-            list.add(i+"");
+            list.add(i + "");
         }
 
         ProductRecycviewAdapter mproduct = new ProductRecycviewAdapter(list);
-        product.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        product.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         product.setAdapter(mproduct);
         return view;
     }
-
 
 
     private void initBanner() {
@@ -162,126 +123,9 @@ public class HomeFragment extends Fragment implements MenuListener, View.OnClick
         });
     }
 
-    @Override
-    public void onMenuOpen() {
+    public void setEntranceAvatar(Bitmap bitmap) {
+        entranceAvatar.setImageBitmap(bitmap);
 
-    }
-
-    @Override
-    public void onMenuClose() {
-
-    }
-
-    @Override
-    public void onProgressUpdate(float value, boolean bouncing) {
-
-    }
-
-
-    public void openAlbum() {
-        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, CROP_PHOTO);
-            } else {
-                ActivityCompat.requestPermissions(getActivity(), perms, REQUEST_PERMISSION);
-
-            }
-        } else {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType("image/*");
-            startActivityForResult(intent, CROP_PHOTO);
-        }
-
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case REQUEST_PERMISSION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openAlbum();
-                }
-
-        }
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case CROP_PHOTO:
-                startActivityForResult(cropPhoto(data.getData()), FINISH_CROP);
-                break;
-            case FINISH_CROP:
-                try {
-                    Bitmap bitmap = data.getParcelableExtra("data");
-                    if (bitmap == null) {
-                        userAvatar.setImageResource(R.mipmap.default_avatar);
-                    } else {
-                        userAvatar.setImageBitmap(bitmap);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                break;
-
-
-        }
-    }
-
-
-    private Intent cropPhoto(Uri oriImageUri) {
-
-        try {
-
-            Intent intent = new Intent("com.android.camera.action.CROP");
-
-            intent.putExtra("crop", true);
-            intent.putExtra("aspectX", 1);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            intent.putExtra("aspectY", 1);
-            intent.putExtra("outputX", ConvertUtils.dp2px(90));
-            intent.putExtra("outputY", ConvertUtils.dp2px(90));
-            intent.putExtra("scale", true);
-            intent.putExtra("return-data", true);
-            intent.setDataAndType(oriImageUri, "image/*");
-
-
-            intent.putExtra("noFaceDetection", true);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-            return intent;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.layout_order_manage:
-                break;
-            case R.id.layout_collection:
-                break;
-            case R.id.layout_customer_service:
-                break;
-            case R.id.layout_setting:
-                break;
-            case R.id.civ_avatar:
-                openAlbum();
-
-                break;
-        }
     }
 
 
